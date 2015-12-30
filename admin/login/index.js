f37e65628ef5addc84529c8fdc7802edde8f4107
Matías Lescano <matiasj.lescano@gmail.com>
2015-12-30 14:20:34 -0300

    add admin login

--- admin/login/index.js
+++ admin/login/index.js
@@ -7,8 +7,32 @@ app.get('/admin/login', function (req, res) {
   res.render('login')
 })
 
-app.post('/admin/login', passport.authenticate('local'), function (req, res) {
-  res.redirect('/admin')
+// app.post('/admin/login', passport.authenticate('local', {
+//   successRedirect: '/admin',
+//   failureRedirect: '/admin/login',
+//   failureFlash: true
+// }))
+
+app.post('/admin/login', function (req, res, next) {
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      console.error(err)
+      return res.redirect('/admin/login')
+    }
+
+    if (!user) {
+      console.log('User not found', req.body)
+      return res.redirect('/admin/login')
+    }
+
+    req.logIn(user, function (err) {
+      if (err) {
+        console.error(err)
+        return res.redirect('/admin/login')
+      }
+      return res.redirect('/admin')
+    })
+  })(req, res, next)
 })
 
 app.get('/logout', function (req, res) {
