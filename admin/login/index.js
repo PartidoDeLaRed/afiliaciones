2a7981b75e76475fcd538294e8567b08bd00e41a
Matías Lescano <matiasj.lescano@gmail.com>
2015-12-30 13:24:21 -0300

    add login view and routes

--- admin/login/index.js
+++ admin/login/index.js
@@ -1,8 +1,19 @@
 var express = require('express')
+var passport = require('passport')
+
 var app = express.Router()
 
-app.get('/admin', function (req, res) {
+app.get('/admin/login', function (req, res) {
   res.render('admin/login')
+})
+
+app.post('/admin/login', passport.authenticate('local'), function (req, res) {
+  res.redirect('/admin')
+})
+
+app.get('/logout', function (req, res) {
+  req.logout()
+  res.redirect('/admin')
 })
 
 module.exports = app
