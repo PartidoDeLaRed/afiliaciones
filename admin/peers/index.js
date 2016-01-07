288dc6cf20a02392337362c54c33306a82c8cbfc
Matías Lescano <matiasj.lescano@gmail.com>
2016-01-07 18:55:55 -0300

    use 2 spaces always

--- admin/peers/index.js
+++ admin/peers/index.js
@@ -5,10 +5,10 @@ var app = express.Router()
 
 app.get('/admin/peers', function (req, res) {
   Peer.find().exec(function (err, peers) {
-    if (err) return res.status(500).send()
-    res.render('peers', {
-      peers: peers
-    })
+  if (err) return res.status(500).send()
+  res.render('peers', {
+    peers: peers
+  })
   })
 })
 
@@ -17,42 +17,42 @@ app.get('/admin/peers/new', function (req, res) {
 })
 
 app.post('/admin/peers', function (req, res) {
-    Peer.create({
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        matricula: {
-            tipo: req.body.matricula_tipo,
-            numero: req.body.matricula_numero
-        },
-        fechaDeNacimiento: req.body.fechaNacimiento,
-        lugarDeNacimiento: req.body.lugarNacimiento,
-        sexo: req.body.sexo,
-        estadoCivil: req.body.estadoCivil,
-        email: req.body.email,
-        telefono: req.body.telefono,
-        profesion: req.body.profesion,
-        domicilio: {
-            calle: req.body.domicilioDoc_calle,
-            numero: req.body.domicilioDoc_numero,
-            piso: req.body.domicilioDoc_piso,
-            depto: req.body.domicilioDoc_depto,
-            codPostal: req.body.domicilioDoc_codPostal,
-            localidad: req.body.domicilioDoc_localidad,
-            provincia: req.body.domicilioDoc_provincia
-        },
-        domicilioReal: {
-            calle: req.body.domicilioReal_calle,
-            numero: req.body.domicilioReal_numero,
-            piso: req.body.domicilioReal_piso,
-            depto: req.body.domicilioReal_depto,
-            codPostal: req.body.domicilioReal_codPostal,
-            localidad: req.body.domicilioReal_localidad,
-            provincia: req.body.domicilioReal_provincia
-        }
-    }, function (err, peer) {
-        if (err) return res.status(500).send(err)
-        res.redirect('/admin/peers')
-    })
+  Peer.create({
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    matricula: {
+      tipo: req.body.matricula_tipo,
+      numero: req.body.matricula_numero
+    },
+    fechaDeNacimiento: req.body.fechaNacimiento,
+    lugarDeNacimiento: req.body.lugarNacimiento,
+    sexo: req.body.sexo,
+    estadoCivil: req.body.estadoCivil,
+    email: req.body.email,
+    telefono: req.body.telefono,
+    profesion: req.body.profesion,
+    domicilio: {
+      calle: req.body.domicilioDoc_calle,
+      numero: req.body.domicilioDoc_numero,
+      piso: req.body.domicilioDoc_piso,
+      depto: req.body.domicilioDoc_depto,
+      codPostal: req.body.domicilioDoc_codPostal,
+      localidad: req.body.domicilioDoc_localidad,
+      provincia: req.body.domicilioDoc_provincia
+    },
+    domicilioReal: {
+      calle: req.body.domicilioReal_calle,
+      numero: req.body.domicilioReal_numero,
+      piso: req.body.domicilioReal_piso,
+      depto: req.body.domicilioReal_depto,
+      codPostal: req.body.domicilioReal_codPostal,
+      localidad: req.body.domicilioReal_localidad,
+      provincia: req.body.domicilioReal_provincia
+    }
+  }, function (err, peer) {
+    if (err) return res.status(500).send(err)
+    res.redirect('/admin/peers')
+  })
 })
 
 module.exports = app
--- public/js/peers/list.js
+++ public/js/peers/list.js
@@ -1,60 +1,57 @@
 ﻿var Peers = [];
 
 $(document).ready(function () {
-    //loadPeers();
-    loadSearchBoxes();
+  //loadPeers();
+  loadSearchBoxes();
 });
 
-function loadPeers()
-{
-    Peers.forEach(function (item) {
-        var container = crearElemento('div', 'peerContainer');
-            container.append(crearElemento('div', 'peerEstado ok'));
-            container.append(crearElemento('div', 'peerNombre').html(item.nombre + ' ' + item.apellido));
-            container.append(crearElemento('div', 'peerMatricula').html(item.matricula.numero));
-            container.append(crearElemento('div', 'peerEmail').html(item.email));
-        
-            var actionsContainer = crearElemento('div', 'actionsContainer');
-                actionsContainer.append(crearElemento('a', 'button delete').click(function () {
-                    //load delete confirmation
-                }));
-                actionsContainer.append(crearElemento('a', 'button edit').click(function () {
-                    //load edit page
-                }));
-            container.append(actionsContainer);
+function loadPeers() {
+  Peers.forEach(function (item) {
+    var container = crearElemento('div', 'peerContainer');
+      container.append(crearElemento('div', 'peerEstado ok'));
+      container.append(crearElemento('div', 'peerNombre').html(item.nombre + ' ' + item.apellido));
+      container.append(crearElemento('div', 'peerMatricula').html(item.matricula.numero));
+      container.append(crearElemento('div', 'peerEmail').html(item.email));
 
-        $('.peersList').append(container);
-    });
-}
-
-function loadSearchBoxes()
-{
-    $('.listHeader:not(.actions)').each(function (index, item) {
-        var searchButton = crearElemento('div', 'button search');
-        var searchContainer = crearElemento('div', 'searchContainer');
-        var searchField = crearElemento('input', 'searchField').attr('type', 'text')
-        searchContainer.append(searchField);
-        searchContainer.append(crearElemento('div', 'button cancel').click(function () {
-            $(searchButton).fadeIn(100);
-            $(searchContainer).fadeOut(100);
+      var actionsContainer = crearElemento('div', 'actionsContainer');
+        actionsContainer.append(crearElemento('a', 'button delete').click(function () {
+          //load delete confirmation
         }));
-        
-        searchButton.click(function () {
-            $(searchButton).fadeOut(100);
-            $(searchContainer).fadeIn(100);
-            $(searchField).value = '';
-            $(searchField).focus();
-        })
+        actionsContainer.append(crearElemento('a', 'button edit').click(function () {
+          //load edit page
+        }));
+      container.append(actionsContainer);
 
-        $(item).append(searchButton);
-        $(item).append(searchContainer);
-        $(searchContainer).fadeOut(100);
-    });
+    $('.peersList').append(container);
+  });
 }
 
-function crearElemento(_tag, _class)
-{
-    var el = document.createElement(_tag);
-    $(el).addClass(_class);
-    return $(el);
+function loadSearchBoxes() {
+  $('.listHeader:not(.actions)').each(function (index, item) {
+    var searchButton = crearElemento('div', 'button search');
+    var searchContainer = crearElemento('div', 'searchContainer');
+    var searchField = crearElemento('input', 'searchField').attr('type', 'text')
+    searchContainer.append(searchField);
+    searchContainer.append(crearElemento('div', 'button cancel').click(function () {
+      $(searchButton).fadeIn(100);
+      $(searchContainer).fadeOut(100);
+    }));
+
+    searchButton.click(function () {
+      $(searchButton).fadeOut(100);
+      $(searchContainer).fadeIn(100);
+      $(searchField).value = '';
+      $(searchField).focus();
+    })
+
+    $(item).append(searchButton);
+    $(item).append(searchContainer);
+    $(searchContainer).fadeOut(100);
+  });
+}
+
+function crearElemento(_tag, _class) {
+  var el = document.createElement(_tag);
+  $(el).addClass(_class);
+  return $(el);
 }
