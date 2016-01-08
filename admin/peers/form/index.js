cd263901ad1bc25e990b0c8f7685d216f27aa54d
Matías Lescano <matiasj.lescano@gmail.com>
2016-01-07 21:23:10 -0300

    add form example

---
+++ admin/peers/form/index.js
@@ -0,0 +1,43 @@
+var form = require('express-form')
+var field = form.field
+
+module.exports = form(
+  field('nombre').trim().is(/^[a-z]+$/),
+  field('apellido').trim().is(/^[0-9]+$/)
+ )
+
+/*
+{
+    nombre: req.body.nombre,
+    apellido: req.body.apellido,
+    matricula: {
+      tipo: req.body.matricula_tipo,
+      numero: req.body.matricula_numero
+    },
+    fechaDeNacimiento: req.body.fechaNacimiento,
+    lugarDeNacimiento: req.body.lugarNacimiento,
+    sexo: req.body.sexo,
+    estadoCivil: req.body.estadoCivil,
+    email: req.body.email,
+    telefono: req.body.telefono,
+    profesion: req.body.profesion,
+    domicilio: {
+      calle: req.body.domicilioDoc_calle,
+      numero: req.body.domicilioDoc_numero,
+      piso: req.body.domicilioDoc_piso,
+      depto: req.body.domicilioDoc_depto,
+      codPostal: req.body.domicilioDoc_codPostal,
+      localidad: req.body.domicilioDoc_localidad,
+      provincia: req.body.domicilioDoc_provincia
+    },
+    domicilioReal: {
+      calle: req.body.domicilioReal_calle,
+      numero: req.body.domicilioReal_numero,
+      piso: req.body.domicilioReal_piso,
+      depto: req.body.domicilioReal_depto,
+      codPostal: req.body.domicilioReal_codPostal,
+      localidad: req.body.domicilioReal_localidad,
+      provincia: req.body.domicilioReal_provincia
+    }
+
+*/
--- admin/peers/index.js
+++ admin/peers/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var Peer = require('../../lib/models').Peer
+var peerForm = require('./form')
 
 var app = express.Router()
 
@@ -16,47 +17,17 @@ app.get('/admin/peers/new', function (req, res) {
   res.render('peers/new')
 })
 
-app.post('/admin/peers', function (req, res) {
-  Peer.create({
-    nombre: req.body.nombre,
-    apellido: req.body.apellido,
-    matricula: {
-      tipo: req.body.matricula_tipo,
-      numero: req.body.matricula_numero
-    },
-    fechaDeNacimiento: req.body.fechaNacimiento,
-    lugarDeNacimiento: req.body.lugarNacimiento,
-    sexo: req.body.sexo,
-    estadoCivil: req.body.estadoCivil,
-    email: req.body.email,
-    telefono: req.body.telefono,
-    profesion: req.body.profesion,
-    domicilio: {
-      calle: req.body.domicilioDoc_calle,
-      numero: req.body.domicilioDoc_numero,
-      piso: req.body.domicilioDoc_piso,
-      depto: req.body.domicilioDoc_depto,
-      codPostal: req.body.domicilioDoc_codPostal,
-      localidad: req.body.domicilioDoc_localidad,
-      provincia: req.body.domicilioDoc_provincia
-    },
-    domicilioReal: {
-      calle: req.body.domicilioReal_calle,
-      numero: req.body.domicilioReal_numero,
-      piso: req.body.domicilioReal_piso,
-      depto: req.body.domicilioReal_depto,
-      codPostal: req.body.domicilioReal_codPostal,
-      localidad: req.body.domicilioReal_localidad,
-      provincia: req.body.domicilioReal_provincia
-    }
-  }, function (err, peer) {
+app.post('/admin/peers', peerForm, function (req, res) {
+  Peer.create(peerForm, function (err, peer) {
     if (err) return res.status(500).send(err)
     res.redirect('/admin/peers')
   })
 })
 
-app.put('/admin/peers', function (req, res) {
-  res.send('PUT /admin/peers')
+app.put('/admin/peers/:id', peerForm, function (req, res) {
+  req.peer.set(req.form).save(function () {
+    res.redirect('/admin/peers')
+  })
 })
 
 module.exports = app
