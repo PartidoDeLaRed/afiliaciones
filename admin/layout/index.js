0ec18aa50e820d85db6c393465d759fc3fac2b68
Federico Villedary <federico.villedary@gmail.com>
2016-01-11 23:08:15 -0300

    Added js to front-end
    - Search boxes in column headers

--- admin/layout/index.js
+++ admin/layout/index.js
@@ -1,1 +1,33 @@
 var $ = require('jquery')
+
+$(document).ready(function () {
+  // loadPeers()
+  loadSearchBoxes()
+})
+
+function loadSearchBoxes() {
+  $('.listHeader:not(.actions, .state)').each(function (index, item) {
+    $(item).children().remove();
+
+    var searchButton = $('<div class="button search" />')
+    
+    var searchContainer = $('<div class="searchContainer" />')
+    var searchField = $('<input type="text" class="searchField" />')
+    searchContainer.append(searchField)
+    searchContainer.append($('<div class="button cancel" />').click(function () {
+      searchButton.fadeIn(100)
+      searchContainer.fadeOut(100)
+    }))
+    
+    searchButton.click(function () {
+      searchButton.fadeOut(100)
+      searchContainer.fadeIn(100)
+      searchField.value = ''
+      searchField.focus()
+    })
+    
+    $(item).append(searchButton)
+    $(item).append(searchContainer)
+    searchContainer.fadeOut(100)
+  })
+}
