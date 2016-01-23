b71848e7bd9fb1dab3e7ff707f70c74ba2f51f4e
Matías Lescano <matiasj.lescano@gmail.com>
2016-01-23 17:11:50 -0300

    add peer create and delete on the API

--- admin/api/index.js
+++ admin/api/index.js
@@ -1,28 +1,7 @@
 var express = require('express')
-var bodyParser = require('body-parser')
-var Peer = require('../../lib/models').Peer
 
 var app = express.Router()
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
-app.use(bodyParser.json())
-
-app.param('id', function (req, res, next, id) {
-  Peer.findOne({'_id': id}).exec(function (err, peer) {
-    if (err) return res.status(400).send()
-    req.peer = peer
-    next()
-  })
-})
-
-app.get('/api/admin/peers', function (req, res) {
-  Peer.find({deletedAt: null}).exec(function (err, peers) {
-    if (err) return res.status(500).send()
-    res.json(peers)
-  })
-})
+app.use(require('./peers'))
 
 module.exports = app
---
+++ admin/api/peers/index.js
@@ -0,0 +1,43 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var Peer = require('../../../lib/models').Peer
+var peerForm = require('./form')
+
+var app = express.Router()
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
+app.param('id', function (req, res, next, id) {
+  Peer.findOne({'_id': id}).exec(function (err, peer) {
+    if (err) return res.status(400).send()
+    req.peer = peer
+    next()
+  })
+})
+
+app.get('/api/admin/peers', function (req, res) {
+  Peer.find({deletedAt: null}).exec(function (err, peers) {
+    if (err) return res.status(500).send()
+    res.json(peers)
+  })
+})
+
+app.post('/admin/peers', peerForm, function (req, res) {
+  Peer.create(req.peer, function (err, peer) {
+    if (err) return res.status(500).send(err)
+    res.status(200).send()
+  })
+})
+
+app.put('/admin/peers/:id', peerForm, function (req, res) {
+  req.peer.save(function (err) {
+    if (err) return res.status(500).send(err)
+    res.status(200).send()
+  })
+})
+
+module.exports = app
