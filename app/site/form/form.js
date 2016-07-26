f2f99231d6d352e1af195f674ad185340ceb82fc
pferder <pferder@gmail.com>
2016-07-26 20:25:00 -0300

    required on multiselect

--- app/site/form/form.js
+++ app/site/form/form.js
@@ -3,9 +3,13 @@ var barrios = require('./barrios.json')
 
 require('selectize')
 
-$('#select-state').selectize({
+var multiSelectBarrios = $('#select-state').selectize({
     options: barrios
 });
+
+var barriosValue = multiSelectBarrios[0].selectize;
+$('#select-state').show();
+$('#select-state').css({ 'height': '0px', 'float': 'left', 'position': 'relative', 'top': '38px', 'left': '150px', 'opacity': '0' });
 
 $(document).ready(function() {
 
@@ -15,13 +19,12 @@ $(document).ready(function() {
 
         data = $(this).serializeArray();
         $.post('/afiliate', data)
-        .success(function() {
-            window.location = '/afiliate/success';
-        })
-        .fail(function(err){
-         	alert(err);
-        })
-
+            .success(function() {
+                window.location = '/afiliate/success';
+            })
+            .fail(function(err) {
+                alert(err);
+            })
 
         return false;
     })
--- app/site/form/index.js
+++ app/site/form/index.js
@@ -4,72 +4,58 @@ var mailer = require('../../shared/mailer')
 
 var app = express.Router()
 
-app.get('/afiliate', function (req, res) {
-  res.render('form')
+app.get('/afiliate', function(req, res) {
+    res.render('form')
 })
 
-app.post('/afiliate', function validate (req, res, next) {
-  req.check('email').notEmpty().isEmail().normalizeEmail()
-  req.check('name').notEmpty().isLength({max: 124})
-  req.check('lastname').optional().isLength({max: 124})
-  req.check('barrios').optional().isArray()
+app.post('/afiliate', function(req, res) {
+    var data = req.body
 
-  req.sanitize('name').trim().escape()
-  req.sanitize('lastname').trim().escape()
-
-  if (req.validationErrors()) {
-    res.status(400).send()
-  } else {
-    next()
-  }
-}, function (req, res) {
-  var data = req.body
-
-  if (data.barrios) {
-    data.barrios = data.barrios.join ? data.barrios.join(', ') : data.barrios
-  }
-
-  app.render('emails/mail-afiliado', data, function (err, html) {
-    if (err) {
-      console.error(err)
-      return
+    if (data.barrios) {
+        data.barrios = data.barrios.join ? data.barrios.join(', ') : data.barrios
     }
 
-    mailer.sendMail({
-      from: config.mailer.sender.name,
-      to: data.email,
-      subject: '¡Bienvenido al Partido de la Red!',
-      html: html
-    }, function (err, info) {
-      if (err) {
-        console.error(err)
-      } else {
-        console.log('Mail "emails/mail-afiliado" sent with data: ', data)
-      }
+    res.render('emails/mail-afiliado', data, function(err, html) {
+        if (err) {
+            console.error(err)
+            return
+        }
+
+        mailer.sendMail({
+            from: config.mailer.sender.name,
+            to: data.email,
+            subject: '¡Bienvenido al Partido de la Red!',
+            html: html
+        }, function(err, info) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log('Mail "emails/mail-afiliado" sent with data: ', data)
+            }
+        })
     })
-  })
 
-  app.render('emails/mail-afiliado-admin', data, function (err, html) {
-    if (err) {
-      console.error(err)
-      return
-    }
+    res.render('emails/mail-afiliado-admin', data, function(err, html) {
+        if (err) {
+            console.error(err)
+            return
+        }
 
-    mailer.sendMail({
-      from: config.mailer.sender.name,
-      to: config.mailer.sender.email,
-      subject: `[${data.barriosString || 'Ningún barrio'}] ${data.name} ${data.lastname}`,
-      html: html
-    }, function (err) {
-      if (err) {
-        console.error(err)
-      } else {
-        console.log('Mail "emails/mail-afiliado-admin" sent with data: ', data)
-      }
+        mailer.sendMail({
+            from: config.mailer.sender.name,
+            to: config.mailer.sender.email,
+            subject: '[${data.barriosString || "Ningún barrio"}] ${data.name} ${data.lastname}',
+            html: html
+        }, function(err) {
+            if (err) {
+                console.error(err)
+            } else {
+                console.log('Mail "emails/mail-afiliado-admin" sent with data: ', data)
+            }
+        })
     })
-  })
 
-  res.status(200).send('ok')
+    res.status(200).send('ok')
 })
 
 module.exports = app
