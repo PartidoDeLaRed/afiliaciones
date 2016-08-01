92f4030d248eb8d07dc952a9769093ee86c3e0ac
Matías Lescano <matiasj.lescano@gmail.com>
2016-08-01 15:28:47 -0300

    use mailer directTransport by default

--- app/shared/mailer/index.js
+++ app/shared/mailer/index.js
@@ -2,8 +2,16 @@ var nodemailer = require('nodemailer')
 var sgTransport = require('nodemailer-sendgrid-transport')
 var config = require('../../config')
 
-module.exports = nodemailer.createTransport(sgTransport({
-  auth: {
-    api_key: config.mailer.key
-  }
-}))
+var transport
+
+if (config.mailer.transport === 'sendgrid') {
+  transport = sgTransport({
+    auth: {
+      api_key: config.mailer.key
+    }
+  })
+} else if (config.mailer.transport) {
+  throw new Error('Invalid config.mailer.transport configuration option.')
+}
+
+module.exports = nodemailer.createTransport(transport)
